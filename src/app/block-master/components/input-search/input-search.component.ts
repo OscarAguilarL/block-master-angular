e21f3180ba808efcae1096858fa04216cd4a3800
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-08-07 11:23:19 -0500

    [Feat] Input search component functionality

--- src/app/block-master/components/input-search/input-search.component.ts
+++ src/app/block-master/components/input-search/input-search.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-input-search',
   templateUrl: './input-search.component.html',
-  styleUrls: ['./input-search.component.css']
+  styleUrls: ['./input-search.component.css'],
 })
 export class InputSearchComponent implements OnInit {
+  termino: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buscar() {
+    this.termino = this.termino.replace(' ', '-')
+    this.router.navigate(['/search', this.termino])
+    this.termino = ''
   }
-
 }
