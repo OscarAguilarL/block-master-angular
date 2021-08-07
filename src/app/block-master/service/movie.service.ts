6b01781270cf78f1b5ed3c1bed79a176163333a7
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-08-07 11:39:38 -0500

    [Feat] Service to get single movie details

--- src/app/block-master/service/movie.service.ts
+++ src/app/block-master/service/movie.service.ts
@@ -1,41 +1,47 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
-import { MovieResponse } from '../interface/movie.interface';
+import { MovieResponse, Movie } from '../interface/movie.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
-  private apiUrl: string = 'https://api.themoviedb.org/3';
-  apiKey: string = '26226bc0d351dd2e356a4f74d1f1d276';
+  private _apiUrl: string = 'https://api.themoviedb.org/3';
+  private _apiKey: string = '26226bc0d351dd2e356a4f74d1f1d276';
 
   get params() {
-    return new HttpParams().set('api_key', this.apiKey);
+    return new HttpParams().set('api_key', this._apiKey);
   }
 
   constructor(private http: HttpClient) {}
 
   getAllMovies(): Observable<MovieResponse> {
-    const url: string = `${this.apiUrl}/movie/popular`;
+    const url: string = `${this._apiUrl}/movie/popular`;
     return this.http.get<MovieResponse>(url, { params: this.params });
   }
 
   getTopRatedMovies(): Observable<MovieResponse> {
-    const url: string = `${this.apiUrl}/movie/top_rated`;
+    const url: string = `${this._apiUrl}/movie/top_rated`;
     return this.http.get<MovieResponse>(url, { params: this.params });
   }
 
   getLessValuedMovies(): Observable<MovieResponse> {
-    const url: string = `${this.apiUrl}/discover/movie`;
+    const url: string = `${this._apiUrl}/discover/movie`;
     return this.http.get<MovieResponse>(url, { params: this.params });
   }
 
-  getSearch( termino: string ): Observable<MovieResponse> {
+  getSearch(termino: string): Observable<MovieResponse> {
     termino = termino.replace('-', ' ');
-    const url: string = `${this.apiUrl}/search/movie`;
-    return this.http.get<MovieResponse>(url, { params: this.params.set('query', termino) });
+    const url: string = `${this._apiUrl}/search/movie`;
+    return this.http.get<MovieResponse>(url, {
+      params: this.params.set('query', termino),
+    });
+  }
+
+  getMovieDetails(movieId: string): Observable<Movie> {
+    const url: string = `${this._apiUrl}/movie/${movieId}`;
+    return this.http.get<Movie>(url, { params: this.params });
   }
 }
